6f32f03a798c4bca90df7b26fa18ba593d93743f
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2017-11-18 17:19:16 -0500

    Client ajax json loading

--- js/recopilacionClientes.js
+++ js/recopilacionClientes.js
@@ -4,10 +4,26 @@ xmlhttp.send();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        console.log(myObj);
+        alert(myObj.Clientes.length);
         var divForInserting = document.getElementById("section_for_Clients");
        	for (var i = myObj.Clientes.length - 1; i >= 0; i--) {
        		var objInside = myObj.Clientes[i]
+       		var divContainer = document.createElement('div');
+       		divContainer.className = "container down";
+       		divForInserting.appendChild(divContainer);
+
+       		var id = document.createElement('p');
+       		id.innerHTML = objInside.id;
+       		var Name = document.createElement('h3');
+       		Name.innerHTML = objInside.Name;
+       		var Description = document.createElement('p');
+       		Description.innerHTML = objInside.Description;
+       		var image = document.createElement('img');
+       		image.src = objInside.image;
+       		divContainer.appendChild(id);
+       		divContainer.appendChild(Name);
+       		divContainer.appendChild(Description);
+       		divContainer.appendChild(image);
 
        	}
 
